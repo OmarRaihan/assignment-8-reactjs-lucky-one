9a83281e04df03a85132230950984d84b2bb92eb
OmarRaihan <omarraihan91@gmail.com>
2022-03-26 20:15:21 +0600

    cart added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,30 +8,30 @@ const Cart = (props) => {
     const {cart} = props;
     console.log(cart);
 
-    let image = ""
-    let name;
-    let brand;
-    for(const product of cart){
-        image = product.img;
-        name = product.name;
-        brand = product.brand;
-    }
-
-
-
     return (
         <div className='cart'>
             <h2 className='text-center my-3'>Cart</h2>
-            <div className='cart-info'>
-                <img src={image} alt="" />
-                <div>
-                <h5>{name}</h5>
-                <p><span>Brand:</span> {brand}</p>
-                </div>
-                <FontAwesomeIcon className='ms-4 trash' icon={faTrashCan}></FontAwesomeIcon>
+            <div >
+                {
+                    cart.map(item => {
+                        
+                        return (
+                            <div className='cart-info col-12 my-2'>
+                                <img src={item.img} alt="" />
+                                <div>
+                                <h5>{item.name}</h5>
+                                <p><span>Brand:</span> {item.brand}</p>
+                                </div>
+                                <FontAwesomeIcon className='ms-auto me-3 trash' icon={faTrashCan}></FontAwesomeIcon>
+                            </div>
+                        )
+                    })
+                }
             </div>
-            <button className='button mx-5'>Select One</button>
-            <button className='button'>Choose Again</button>
+            <div className='text-center ms-auto'>
+            <button className='button text-center'>Select One</button>
+            <button className='button C'>Choose Again</button>
+            </div>
         </div>
     );
 };
