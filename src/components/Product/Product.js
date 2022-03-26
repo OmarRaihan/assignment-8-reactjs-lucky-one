0d70c0ed5afe4beb9c1699bb4b8fccdbfa895bbc
OmarRaihan <omarraihan91@gmail.com>
2022-03-26 09:15:08 +0600

    name added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './Product.css'
+
+const Product = () => {
+    return (
+        <div>
+            <h3>Laptop Gallery</h3>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Products/Products.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './Products.css'
+
+const Products = () => {
+    return (
+        <div>
+            <div className='headline text-center align-items-center w-50 mx-auto'>
+            <h1>Laptop City</h1>
+            <p >Welcome to Laptop City. Grab the best laptop for you.</p>
+            </div>
+
+            {/* product section */}
+        </div>
+    );
+};
+
+export default Products;
